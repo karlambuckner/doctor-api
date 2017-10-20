850efa9c9bad211fc9e6a0ee37fa671a86907fec
Karla Buckner <kmb2662@gmail.com>
2017-10-20 16:27:46 -0700

    add API and JS functionality, add karma and env file, finishing touches

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,4 +1,15 @@
-const apiKey = require('./../.env').apiKey;
-import {Doctor} from './../js/doctor.js';
+import { Doctor } from './../js/doctor.js';
+let apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
+
+  $('#doctor-form').submit(function(event) {
+    event.preventDefault();
+    let doctor = new Doctor();
+    let issue = $('#issue').val();
+    let search = $('#search').val();
+    let promise = doctor.initiatePromise(`https://api.betterdoctor.com/2016-03-01/doctors?last_name=${search}&query=${issue}&location=45.523%2C-122.676%2C100&user_location=45.523%2C-122.676&fields=practices(name%2Caccepts_new_patients%2Cvisit_address(city%2Cstate_long%2Cstreet%2Czip)%2Cphones(number))%2Cprofile(first_name%2C%20middle_name%2C%20last_name%2Cimage_url%2Cbio)&skip=0&limit=10&user_key=${apiKey}`);
+
+    doctor.getApi(promise);
+  });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -1,37 +1,47 @@
 export class Doctor {
-  doctorByIssue(issue) {
-      let promise = new Promise(function(resolve, reject) {
-        let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`;
-        console.log(url);
-        request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          } else {
-            reject(Error(request.response));
-          }
-        };
 
-        request.open("GET", url, true);
-        request.send();
-      });
-   }
+  initiatePromise(api_key){
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = api_key;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 
-  doctorBySearch(search) {
-      let promise = new Promise(function(resolve, reject) {
-        let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`;
-        console.log(url);
-        request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          } else {
-            reject(Error(request.response));
-          }
-        };
-
-        request.open("GET", url, true);
-        request.send();
-      });
+  getApi(promise){
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      if(body.meta.total > 0){
+        for(let i = 0; i < body.data.length +1; ++i){
+        $('#output').append("Name: <h3>Dr. " + body.data[i].profile.first_name + " " + body.data[i].profile.last_name +"</h3>");
+        $('#output').append("Practice: <h4>" + body.data[i].practices[0].name + "</h4>");
+        $('#output').append("Phone: <h4>" + body.data[i].practices[0].phones[0].number + "</h4>");
+        $('#output').append("Street: <h4>" + body.data[i].practices[0].visit_address.street + "</h4>");
+        $('#output').append("City: <h4>" + body.data[i].practices[0].visit_address.city + "</h4>");
+        $('#output').append("State: <h4>" + body.data[i].practices[0].visit_address.state_long + "</h4>");
+        $('#output').append("Zip Code: <h4>" + body.data[i].practices[0].visit_address.zip + "</h4>");
+        if(body.data[i].practices[0].accepts_new_patients){
+          $('#output').append("Accepting new patients: <h4> Yes</h4>");
+        }
+        else{
+          $('#output').append("Accepting new patients: <h4>No</h4>");
+        }
+      }
+    }
+    else{
+      $('#output').append("<div class='error1'><h4>Hmm, couldn't find a doctor that matches this search query</h4></div><br>");
+    }
+  },
+    function(error) {
+      $('#error').text(`There was an error processing your request: ${error.message}`);
+    });
   }
 }
