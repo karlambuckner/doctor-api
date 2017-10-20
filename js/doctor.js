0d30de22058649ba93be6fb31106a3327a6102b7
Karla Buckner <kmb2662@gmail.com>
2017-10-20 11:49:48 -0700

    apply new font and styling, begin to add API

--- js/doctor.js
+++ js/doctor.js
@@ -1,1 +1,37 @@
 export class Doctor {
+  doctorByIssue(issue) {
+      let promise = new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`;
+        console.log(url);
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.response));
+          }
+        };
+
+        request.open("GET", url, true);
+        request.send();
+      });
+   }
+
+  doctorBySearch(search) {
+      let promise = new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`;
+        console.log(url);
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.response));
+          }
+        };
+
+        request.open("GET", url, true);
+        request.send();
+      });
+  }
+}
